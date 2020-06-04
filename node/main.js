03bd004dd721169ca9f4ffeebb87eed9a48be4f4
Chung-Sheng Wu <samsam2310@gmail.com>
2020-06-05 03:32:47 +0900

    Add demo page

---
+++ node/main.js
@@ -0,0 +1,19 @@
+const { createCanvas, loadImage } = require('canvas');
+const { scanImageData } = require('zbar.wasm');
+
+const getImageData = async (src) => {
+  const img = await loadImage(src);
+  const canvas = createCanvas(img.width, img.height);
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+  return ctx.getImageData(0, 0, img.width, img.height);
+};
+
+const main = async () => {
+	const img = await getImageData(__dirname + '/test.png');
+	const res = await scanImageData(img);
+	console.log(res[0].typeName); // ZBAR_QRCODE
+	console.log(res[0].decode()); // Hello World
+};
+
+main();
---
+++ src/index.js
@@ -0,0 +1,113 @@
+import { scanImageData } from 'zbar.wasm';
+
+const SCAN_PROID_MS = 800;
+
+const handleResize = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const video = document.getElementById('video');
+  video.width = width;
+  video.height = height;
+
+  const canvas = document.getElementById('canvas');
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  if (width / video.videoWidth < height / video.videoHeight) {
+    canvas.style.width = '100vw';
+    canvas.style.height = 'auto';
+  } else {
+    canvas.style.width = 'auto';
+    canvas.style.height = '100vh';
+  }
+};
+
+const init = async () => {
+  window.onresize = handleResize;
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: 'environment',
+        width: { max: 640 },
+        height: { max: 640 }
+      }
+    });
+  const video = document.getElementById('video');
+  video.srcObject = mediaStream;
+  video.play();
+  await new Promise(r => {
+    video.onloadedmetadata = r;
+  });
+  handleResize();
+};
+
+const render = (symbols) => {
+  const canvas = document.getElementById('canvas');
+  const footer = document.getElementById('footer');
+  const ctx = canvas.getContext('2d');
+  const width = canvas.width;
+  const height = canvas.height;
+  ctx.clearRect(0, 0, width, height);
+  while (footer.firstChild) {
+    footer.removeChild(footer.lastChild);
+  }
+  ctx.font = '20px serif';
+  ctx.strokeStyle = '#00ff00';
+  ctx.fillStyle = '#ff0000';
+  ctx.lineWidth = 6;
+  for (let i = 0; i < symbols.length; ++i) {
+    const sym = symbols[i];
+    const points = sym.points;
+    ctx.beginPath();
+    for (let j = 0; j < points.length; ++j) {
+      const { x, y } = points[j];
+      if (j == 0) {
+        ctx.moveTo(x, y);
+      } else {
+        ctx.lineTo(x, y);
+      }
+    }
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fillText('#' + i, points[0].x, points[0].y - 10);
+
+    const div = document.createElement('div');
+    div.className = 'footerItem';
+    div.innerText = `#${i}: Type: ${sym.typeName}; Value: "${sym.decode()}"`
+    footer.appendChild(div);
+  }
+};
+
+const scan = async () => {
+  const canvas = document.createElement('canvas');
+  const video = document.getElementById('video');
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(video, 0, 0, width, height);
+  const imgData = ctx.getImageData(0, 0, width, height);
+  const res = await scanImageData(imgData);
+  // console.log(res, Date.now());
+  render(res);
+};
+
+const sleep = ms => new Promise(r => { setTimeout(r, ms) });
+
+const main = async () => {
+  try {
+    await init();
+    while (true) {
+      await scan();
+      await sleep(SCAN_PROID_MS);
+    }
+  } catch (err) {
+    const h1 = document.createElement('h1');
+    h1.style = 'position: absolute; top: 20px; left: 20px';
+    h1.innerText = 'Cannot get cammera: ' + err;
+    document.body.appendChild(h1);
+    console.log(err);
+  }
+};
+
+main();
