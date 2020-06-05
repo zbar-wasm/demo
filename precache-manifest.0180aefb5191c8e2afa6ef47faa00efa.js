e663fbbe49f0af3615046911f29566d427cf42b5
Chung-Sheng Wu <samsam2310@gmail.com>
2020-06-05 23:04:45 +0900

    Updates

---
+++ precache-manifest.0180aefb5191c8e2afa6ef47faa00efa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a64bb75520bb277f9bc1bdf9fab092",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "581e229200c258432f40",
+    "url": "/demo/static/js/main.0f326a76.chunk.js"
+  },
+  {
+    "revision": "291c6bec69179f1e0dca",
+    "url": "/demo/static/js/runtime-main.134dfdfb.js"
+  },
+  {
+    "revision": "1476250b0f151ed953afded81ada97da",
+    "url": "/demo/static/media/zbar.wasm.1476250b.bin"
+  }
+]);
